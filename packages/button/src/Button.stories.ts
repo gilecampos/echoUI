3b4704dc995c1a47b91c740606bce19f43e75399
Gileandro Campos <gileandrocampos8@gmail.com>
2024-07-21 16:43:35 -0300

    add button component

--- packages/button/src/Button.stories.ts
+++ packages/button/src/Button.stories.ts
@@ -7,16 +7,19 @@ const meta = {
   title: 'Example/Button',
   component: Button,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    backgrounds: {
+      values: [
+        { name: 'light', value: '#fff' },
+        { name: 'dark', value: '#333' },
+      ],
+    },
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    // backgroundColor: { control: 'color' },
   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+
   args: { onClick: fn() },
 } satisfies Meta<typeof Button>;
 
@@ -26,13 +29,38 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
+    type: 'primary',
     label: 'Button',
   },
 };
 
 export const Secondary: Story = {
   args: {
+    type: 'secondary',
+    label: 'Button',
+  },
+};
+
+export const Style: Story = {
+  args: {
+    label: 'Button',
+    style: {
+      backgroundColor: "red", 
+      color: "yellow"
+    }
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: 'small',
+    label: 'Button',
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    size: 'medium',
     label: 'Button',
   },
 };
@@ -44,9 +72,3 @@ export const Large: Story = {
   },
 };
 
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
-};
--- packages/button/src/Button.tsx
+++ packages/button/src/Button.tsx
@@ -2,45 +2,31 @@ import React from 'react';
 import './button.css';
 
 export interface ButtonProps {
-  /**
-   *
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
+  type?: string;
+  style?: object;
   size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
+  className?: string;
   label: string;
-  /**
-   * Optional click handler
-   */
+  disabled?: boolean;
   onClick?: () => void;
 }
 
-/**
- * Primary UI component for user interaction
- */
 export const Button = ({
-  primary = false,
+  type = 'primary',
   size = 'medium',
-  backgroundColor,
+  style,
+  className,
   label,
+  disabled,
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
+      className={['button', `button--${size}`, `button--${type}`, `${className}`].join(' ')}
+      style={style}
       {...props}
+      disabled={disabled}
     >
       {label}
     </button>
